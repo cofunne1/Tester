867d2aa5d3de622e66d5ca1406367408db63d143
Cee Ofunne <ceeofunne@Cees-MacBook-Pro.local>
2017-06-19 01:04:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ceeofunne/tester/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ceeofunne.tester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+}
